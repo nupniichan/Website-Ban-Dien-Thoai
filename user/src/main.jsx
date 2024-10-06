86ad028ad3cc710147c9770c0e0cbc7285748327
Tequinzyy <tonyminhquan450@gmail.com>
2024-10-06 22:11:29 +0700

    fixing node modules git history

--- user/src/main.jsx
+++ user/src/main.jsx
@@ -1,7 +1,7 @@
+import { ConfigProvider } from "antd";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ConfigProvider } from "antd";
 import App from "./App.jsx";
 import "./index.css";
 
