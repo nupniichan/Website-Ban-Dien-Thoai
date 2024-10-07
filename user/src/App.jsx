2e27ba8a0451f1c7825a6987c15227a632b07089
Tequinzyy <tonyminhquan450@gmail.com>
2024-10-08 03:02:33 +0700

    😨

--- user/src/App.jsx
+++ user/src/App.jsx
@@ -1,24 +1,27 @@
 import { Route, Routes } from "react-router-dom";
-import Header from "./Header&Footer/Header";
-import Footer from "./Header&Footer/Footer";
-import Homepage from "./pages/Homepage";
-import About from "./pages/About";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
-import SearchResults from "./pages/SearchResults";
-import ProductDetails from "./pages/ProductDetails";
-import Profile from "./pages/Profile";
-import MyOrders from "./pages/MyOrders";
-import PathNames from "./PathNames";
+import Footer from "./Header&Footer/Footer.jsx";
+import Header from "./Header&Footer/Header.jsx";
+import About from "./pages/About.jsx";
+import Cart from "./pages/Cart.jsx";
+import Checkout from "./pages/Checkout.jsx";
+import Homepage from "./pages/Homepage.jsx";
+import MyOrders from "./pages/MyOrders.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
+import Profile from "./pages/Profile.jsx";
+import SearchResults from "./pages/SearchResults.jsx";
+import Support from "./pages/Support.jsx";
+import PathNames from "./PathNames.js";
 import "./App.css";
 
 function App() {
     return (
         <>
             <Header />
+
             <Routes>
                 <Route path={PathNames.HOMEPAGE} element={<Homepage />} />
                 <Route path={PathNames.ABOUT} element={<About />} />
+                <Route path={PathNames.SUPPORT} element={<Support />} />
                 <Route path={PathNames.CART} element={<Cart />} />
                 <Route path={PathNames.CHECKOUT} element={<Checkout />} />
                 <Route path={PathNames.PRODUCT_DETAILS} element={<ProductDetails />} />
@@ -26,6 +29,7 @@ function App() {
                 <Route path={PathNames.PROFILE} element={<Profile />} />
                 <Route path={PathNames.MY_ORDERS} element={<MyOrders />} />
             </Routes>
+
             <Footer />
         </>
     );
--- user/src/Header&Footer/Header.jsx
+++ user/src/Header&Footer/Header.jsx
@@ -1,29 +1,79 @@
 import { Link } from "react-router-dom";
-import NeonSign from "../components/BrandLogos/NeonSign";
-import UserMenu from "../components/UserMenu";
+import NeonSign from "../assets/BrandLogos/NeonSign.jsx";
+import UserMenu from "../components/UserMenu.jsx";
+import PathNames from "../PathNames.js";
 import "./Header.css";
+import { SearchOutlined } from "@ant-design/icons";
+
+const MenuItems = [
+    {
+        id: '1',
+        name: "Home",
+        url: `${PathNames.HOMEPAGE}`
+    },
+    {
+        id: '2',
+        name: "Products",
+        url: `${PathNames.PRODUCTS}`
+    },
+    {
+        id: '3',
+        name: "About",
+        url: `${PathNames.ABOUT}`
+    },
+    {
+        id: '4',
+        name: "Support",
+        url: `${PathNames.SUPPORT}`
+    }
+];
 
 const Header = () => {
-    return (
-        // <header className="fixed top-0 w-full shadow-md z-50">
-        //     <div className="header-content-container flex items-center flex-wrap lg:justify-center gap-4  border-b min-h-[75px]">
-        //         <div className="header-left text-orange-600">left section</div>
-        //             <NeonSign text="PHONY BALONEY" />
-        //         <div className="header-right text-orange-600">right section</div>
-        //     </div>
-        // </header>
 
-        <header>
-            <div>
-                <div className="container">
-                    {/* Logo and Links section */}
-                    <div>
-                        <Link to="#" className="">
+
+    return (
+        <header className="bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
+            <div className="py-4">
+                <div className="container flex justify-between items-center">
+                    {/* Logo & Menu section */}
+                    <div className="flex item-center gap-4">
+                        <Link to={`${PathNames.HOMEPAGE}`}>
                             <NeonSign text="PHONY BALONEY" />
                         </Link>
+
+                        {/* Menu items */}
+                        <div className="lg-block">
+                            <ul className="flex items-center gap-4">
+                                {MenuItems.map((item, index) => {
+                                    return (
+                                        <li key={index}>
+                                            <Link to={item.url} className="inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200">
+                                                {item.name}
+                                            </Link>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </div>
                     </div>
 
-                    {/* User menu and search section */}
+                    {/* Header right section */}
+                    <div className="flex justify-between items-center gap-4">
+                        {/* Search */}
+                        <div className="relative group hidden sm:block">
+                            <input type="text" placeholder="Search" className="search-bar"/>
+                            <SearchOutlined className="text-xl text-gray-600 dark:text-gray-400"/>
+                        </div>
+
+                        {/* Cart */}
+
+                        {/* User Menu */}
+                        <div>
+                            <UserMenu/>
+                        </div>
+
+                        {/* Dark mode toggle */}
+                    </div>
                 </div>
             </div>
         </header>
