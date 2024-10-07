18e9845373a42cc21b93a516073cb4fe06539549
Tequinzyy <tonyminhquan450@gmail.com>
2024-10-08 03:31:32 +0700

    fuz

--- user/src/Header&Footer/Header.jsx
+++ user/src/Header&Footer/Header.jsx
@@ -4,32 +4,37 @@ import UserMenu from "../components/UserMenu.jsx";
 import PathNames from "../PathNames.js";
 import "./Header.css";
 import { SearchOutlined } from "@ant-design/icons";
+import { useState } from "react";
 
 const MenuItems = [
     {
-        id: '1',
+        id: "1",
         name: "Home",
-        url: `${PathNames.HOMEPAGE}`
+        url: `${PathNames.HOMEPAGE}`,
     },
     {
-        id: '2',
+        id: "2",
         name: "Products",
-        url: `${PathNames.PRODUCTS}`
+        url: `${PathNames.PRODUCTS}`,
     },
     {
-        id: '3',
+        id: "3",
         name: "About",
-        url: `${PathNames.ABOUT}`
+        url: `${PathNames.ABOUT}`,
     },
     {
-        id: '4',
+        id: "4",
         name: "Support",
-        url: `${PathNames.SUPPORT}`
-    }
+        url: `${PathNames.SUPPORT}`,
+    },
 ];
 
 const Header = () => {
+    const [isSearchExpanded, setSearchExpanded] = useState(false);
 
+    const toggleSearch = () => {
+        setSearchExpanded((prev) => !prev);
+    };
 
     return (
         <header className="bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
@@ -47,7 +52,10 @@ const Header = () => {
                                 {MenuItems.map((item, index) => {
                                     return (
                                         <li key={index}>
-                                            <Link to={item.url} className="inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200">
+                                            <Link
+                                                to={item.url}
+                                                className="inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200"
+                                            >
                                                 {item.name}
                                             </Link>
                                         </li>
@@ -61,15 +69,25 @@ const Header = () => {
                     <div className="flex justify-between items-center gap-4">
                         {/* Search */}
                         <div className="relative group hidden sm:block">
-                            <input type="text" placeholder="Search" className="search-bar"/>
-                            <SearchOutlined className="text-xl text-gray-600 dark:text-gray-400"/>
+                            <input
+                                type="text"
+                                placeholder="What are you looking for?"
+                                className={`search-bar transition-all duration-300 rounded-full px-3 py-1 focus:outline-none dark:bg-gray-900
+                                    ${isSearchExpanded
+                                        ? "w-[300px] border border-gray-500 dark:border-gray-800 dark:bg-gray-800"
+                                        : "w-0"
+                                }`}
+                            />
+                            <SearchOutlined
+                                className="text-xl text-gray-600 group-hover:text-primary dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 duration-200"
+                                onClick={toggleSearch}/>
                         </div>
 
                         {/* Cart */}
 
                         {/* User Menu */}
                         <div>
-                            <UserMenu/>
+                            <UserMenu />
                         </div>
 
                         {/* Dark mode toggle */}
