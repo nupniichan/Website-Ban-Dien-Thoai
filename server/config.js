9e871ee4895f292266234f8b7a6d0b0a4182a067
nup chan <bao96065@gmail.com>
2024-10-10 15:22:43 +0700

    chore: fix ipnUrl

--- server/config.js
+++ server/config.js
@@ -4,7 +4,7 @@ module.exports = {
   orderInfo: 'Thanh toán SPhoneC',
   partnerCode: 'MOMO',
   redirectUrl: 'http://8.219.153.7:5173/payment-result',
-  ipnUrl: 'http://http://8.219.153.7/callback', // tui dung thang luon ip public cua server
+  ipnUrl: 'http://8.219.153.7/callback', // tui dung thang luon ip public cua server
   requestType: 'payWithMethod',
   extraData: '',
   orderGroupId: '',
