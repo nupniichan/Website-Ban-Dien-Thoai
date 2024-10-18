6102dbf6ba53061d32dbdf84d8d71aa12c8ac3e5
nup chan <bao96065@gmail.com>
2024-10-18 10:33:48 +0700

    pull develop branch and change ip

--- server/config.js
+++ server/config.js
@@ -3,8 +3,8 @@ module.exports = {
   secretKey: 'K951B6PE1waDMi640xX08PD3vg6EkVlz',
   orderInfo: 'Thanh toán SPhoneC',
   partnerCode: 'MOMO',
-  redirectUrl: 'http://localhost:5173/payment-result',
-  ipnUrl: 'http://localhost:5000/callback', // Tren develop thi xai ngrok
+  redirectUrl: 'http://4.242.20.80:5173/payment-result',
+  ipnUrl: 'http://4.242.20.80:5000/callback', // Tren develop thi xai ngrok
   requestType: 'payWithMethod',
   extraData: '',
   orderGroupId: '',
