39f4e16c6120a6044bb493653eb5898417fd78f9
nup chan <bao96065@gmail.com>
2024-10-18 11:00:38 +0700

    change ip to localhost

--- user/src/pages/PaymentFailed.jsx
+++ user/src/pages/PaymentFailed.jsx
@@ -1,9 +1,16 @@
+import pageNames from '../PathNames'
+import { useNavigate } from "react-router";
+
 const PaymentFailed = () => {
+  const navigate = useNavigate();
+  const handleToCartPage = () => {
+    navigate(pageNames.CART)
+  }
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-red-600 mb-4">Thanh toán thất bại</h1>
       <p className="text-lg text-gray-700 mb-6">Giao dịch của bạn đã bị từ chối. Vui lòng thử lại.</p>
-      <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+      <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700" onClick={handleToCartPage}>
         Quay về trang giỏ hàng
       </button>
     </div>
--- user/src/pages/PaymentSuccess.jsx
+++ user/src/pages/PaymentSuccess.jsx
@@ -1,13 +1,23 @@
+import pageNames from '../PathNames'
+import { useNavigate } from "react-router";
+
 const PaymentSuccess = () => {
+  const navigate = useNavigate();
+  const handleToPurchasePage = () => {
+    navigate('/user/my-orders')
+  }
+  const handleToHomePage = () => {
+    navigate(pageNames.HOMEPAGE)
+  }
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-green-50">
       <h1 className="text-4xl font-bold text-green-600 mb-4">Thanh toán thành công!</h1>
       <p className="text-lg text-gray-700 mb-6">Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi.</p>
       <div className="flex space-x-4">
-        <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+        <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700" onClick={handleToPurchasePage}>
           Xem lịch sử mua hàng
         </button>
-        <button className="px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-700">
+        <button className="px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-700" onClick={handleToHomePage}>
           Quay về trang chủ
         </button>
       </div>
